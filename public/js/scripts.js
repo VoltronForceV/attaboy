1be59e7bf5b122131dd13cc24654e70d00702fb7
devnill <devnill@gmail.com>
2013-06-01 09:39:35 -0600

    added scripts

---
+++ public/js/scripts.js
@@ -0,0 +1,26 @@
+window.attaboy = (function(window){
+    'use strict';
+    var is_init=0,
+	data={
+
+	},
+	goal={
+	    add: function(){
+
+	    },
+	    find: function(){
+
+	    }
+	},
+	user={
+	    info: function(){
+
+	    }
+	},
+	init={
+
+	};
+    return {
+
+    };
+})(window);
