ff59c05419f16a4172bd4ae07086473b00d3ab67
devnill <devnill@gmail.com>
2013-06-01 17:15:02 -0600

    added goals

---
+++ routes/goal.js
@@ -0,0 +1,95 @@
+exports.goal = {
+    add: function(req, res){
+	if(req.body!== undefined){
+	    //goal_id
+	    var i,err={},user_id = session.username,form_fields={
+		parent_id:undefined,
+		location_id:undefined,
+		date:undefined,
+		duration:undefined,
+		goal_title:undefined,
+		goal_text:undefined,
+		reward:undefined,
+		max_participants:undefined,
+		verification_method:undefined,
+		verification_users:undefined,
+		visibility:undefined
+	    },
+		respond = function(err, response){
+		    var status;
+		    if(err!==undefined){
+			res.send({
+			    status : 'failure',
+			    data   : 'response'
+			});
+		    }
+		    else{
+			res.send({
+			    status : 'success',
+			    data   : 'response'
+			});
+		    }
+		};
+	    for(i in form_fields){
+		if(req.body[i]!==undefined){
+		    form_fields[i]=req.body[i];
+		}
+		else{
+		    if(i==('goal_title'||'goal_text')){
+			err['missing'].push(i);
+		    }
+		    else{
+			//populate with defaults
+			switch(i){
+			case 'parent_id':
+			    form_fields[i]=0;
+			    break;
+			case 'location_id':
+			    form_fields[i]=0;
+			    break;
+			case 'date':
+			    form_fields[i]=0;
+			    break;
+			case 'duration':
+			    form_fields[i]=0;
+			    break;
+			case 'max_participants':
+			    form_fields[i]=0;
+			    break;
+			case 'reward':
+			    form_fields[i]="That warm fuzzy feeling <3";
+			    break;
+			case 'verification_method':
+			    form_fields[i]='creator';
+			    break;
+			case 'verification_users':
+			    form_fields[i]='1';
+			    break;
+			case 'visibility':
+			    form_fields[i]='public';
+			    break;
+			}
+		    }
+		}
+	    } //end post iteration
+
+	    if(err!==undefined){
+		respond(true,err);
+	    }
+	    else{
+		goal.add({
+		    'start_date': 'NOW()',
+		    'end_date': 'DATE_ADD(NOW(), INTERVAL 2 HOUR)',
+		    'goal_title': "Win the Hackathon",
+		    'goal_text': "This isn't sample data, it's a prophecy.",
+		    'max_participants': 5,
+		    'verification_method': '3rd Party',
+		    'verification_users': 3,
+		    'privacy_type': 'Public'
+		}, function(err,goal_id){
+		    respond(err,goal_id);
+		});
+	    }
+	}
+    }
+};
