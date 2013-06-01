f21295bf482aa40bd6e8a893faa8444011cec145
devnill <devnill@gmail.com>
2013-06-01 11:35:40 -0600

    yep

---
+++ public/js/scripts.js
@@ -0,0 +1,28 @@
+window.attaboy = (function(window){
+    'use strict';
+    var is_init=0,
+	data={
+
+	},
+	goal={
+	    add: function(){
+
+	    },
+	    find: function(){
+
+	    }
+	},
+	user={
+	    info: function(){
+
+	    }
+	},
+	init={
+
+	};
+    return {
+	init: function(){
+
+	}
+    };
+})(window);
