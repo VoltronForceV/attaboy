4dde987309c902c4ed4495e4119b4319d00810a5
Alan Baldwin <abaldwin@easy-insight.com>
2013-06-01 23:00:20 -0600

    logout problem fixed

--- routes/login.js
+++ routes/login.js
@@ -9,7 +9,7 @@ exports.login = function(req, res) {
     users.get({user_name: params.user }, function(e, user) {
 	if(user != null) {
             req.session.user = user;
-            res.render("index", {user: user});
+            res.render("dashboard", {user: user});
         } else {
             res.render("login/new", {error: true});
         }
