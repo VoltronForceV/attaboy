9cc1451950dbd3908f66bf9e97fbbd43faa89b76
devnill <devnill@gmail.com>
2013-06-01 19:26:22 -0600

    routes

--- routes/user.js
+++ routes/user.js
@@ -26,7 +26,35 @@ var info = function(req,res){
     }
 },
     update = function(req,res){
-
+	var i;
+	if(req.session.user!==undefined){
+	    if(req.body!=undefined){
+		var profile_update={
+			user_name: req.session.user.user_id,
+			location_id: undefined,
+			user_name: undefined,
+			user_email: undefined,
+			join_date: undefined,
+			login_date: undefined
+		};
+		for(i in req.body){
+		    if(i!='user_id' && profile_update.hasOwnProperty(i)){
+			profile_update[i]=req.body[i];
+		    }
+		}
+		user_model.update(profile_update, function(err, result){
+		    respond(err,result);
+		});
+	    }
+	    else{
+		//no data
+		respond(true,'no data supplied');
+	    }
+	}
+	else{
+	    respond(true, "not logged in");
+	    //not logged in
+	}
     };
 module.exports={
     info   : info,
