e338f8b50c04d39217477d41abefde1d60a8ea5a
devnill <devnill@gmail.com>
2013-06-01 20:21:31 -0600

    added cookie secret

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
+app.use(express.cookieParser(config.cookie_secret));
 app.use(express.session());
 app.use(routes.login.checkLogin);
 app.use(app.router);
